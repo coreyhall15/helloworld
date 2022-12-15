345461e0781e69d819c92e11a71ca58fefd1981f
Corey Hall <cohall15@gmail.com>
2022-12-14 22:03:30 -0500

    created board game with React

---
+++ src/components/Heading.js
@@ -0,0 +1,11 @@
+function Heading(props){
+
+    console.log(props)
+
+    //const jsx = <h2>Goodbye World</h2>
+  
+    return <><h1>{props.cheese}</h1><h3>{props.bread}</h3></>
+  }
+
+// module.exports = heading
+export default Heading
